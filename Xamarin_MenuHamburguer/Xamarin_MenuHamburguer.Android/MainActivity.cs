1f32c56a209e10058f85195f530c61bd5104ec30
dideco1 <diegobaro.si@outlook.com>
2022-10-19 12:06:46 -0300

    Mudando cor da status bar do android

--- Xamarin_MenuHamburguer/Xamarin_MenuHamburguer.Android/MainActivity.cs
+++ Xamarin_MenuHamburguer/Xamarin_MenuHamburguer.Android/MainActivity.cs
@@ -17,6 +17,7 @@ namespace Xamarin_MenuHamburguer.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+            Window.SetStatusBarColor(Android.Graphics.Color.DarkGray);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
